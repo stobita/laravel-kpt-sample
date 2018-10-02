64fdb8320f679ed8bba55e2571bd9b6a0ec946aa
stobita <tobita.esc@gmail.com>
2018-10-03 00:34:10 +0900

    make list

--- resources/js/store.js
+++ resources/js/store.js
@@ -3,6 +3,11 @@ import Vuex from "vuex";
 
 Vue.use(Vuex);
 
+const KEEP_STATUS = 0;
+const PROBLEM_STATUS = 1;
+const TRY_STATUS = 2;
+const DONE_STATUS = 3;
+
 export default new Vuex.Store({
   state: {
     kpt: []
@@ -15,7 +20,24 @@ export default new Vuex.Store({
   actions: {
     fetchKPT({ commit }) {
       this.$http.get("/api/kpts").then(response => {
-        commit("setKPT", response);
+        commit("setKPT", response.data);
+      });
+    }
+  },
+  getters: {
+    getKeep: state => {
+      return state.kpt.filter(item => {
+        return item.status === KEEP_STATUS;
+      });
+    },
+    getProblem: state => {
+      return state.kpt.filter(item => {
+        return item.status === PROBLEM_STATUS;
+      });
+    },
+    getTry: state => {
+      return state.kpt.filter(item => {
+        return item.status === TRY_STATUS;
       });
     }
   }
--- resources/js/views/KPT.vue
+++ resources/js/views/KPT.vue
@@ -1,17 +1,40 @@
 <template>
-  <div>aaa</div>
+  <div>
+    <div v-for="item in keepList" class="keep">
+      {{item.content}}
+    </div>
+    <div v-for="item in problemList" class="problem">
+      {{item.content}}
+    </div>
+    <div v-for="item in tryList" class="try">
+      {{item.content}}
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   created() {
-    this.fetchKPT()
+    this.fetchKPT();
+  },
+  computed: {
+    keepList() {
+      console.log("&&&&&&&&");
+      console.log(this.$store.getters.getKeep);
+      console.log("88888888");
+      return this.$store.getters.getKeep;
+    },
+    problemList() {
+      return this.$store.getters.getProblem;
+    },
+    tryList() {
+      return this.$store.getters.getTry;
+    }
   },
   methods: {
     fetchKPT() {
-      this.$store.dispatch('fetchKPT')
+      this.$store.dispatch("fetchKPT");
     }
   }
-}
-
+};
 </script>
